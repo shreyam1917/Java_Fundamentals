059b14c0b3184fa25016441e5a6ac740d02ee3c1
shreyam1917 <shreyam.111.official@gmail.com>
2023-02-05 01:12:00 +0530

    Day 14

---
+++ DataTypes.java
@@ -0,0 +1,40 @@
+package fundamentals;
+
+public class DataTypes {
+public static void main(String[] args) {
+                  	//FOR INTEGERS
+	
+	// int has 4 bytes which  is equal to 32 bits
+	int a = 4;
+	
+	// short has 2 bytes which is equal to 16 bits
+	short b =2;
+	
+	// byte has 1 byte which is equal to 8 bits
+	byte c = 6;
+	
+	// long has 8 bytes which is equal to 64 bits
+	long d = 8;
+	
+	                 //FOR DECIMALS
+	
+	//Float has 4 bytes which is equal to 32 bits
+	float e = 10.5f;
+	
+	//Double has 8 bytes which is equal to 64 bits
+	double f = 12.5f;
+	
+	//FOR CHARACTERS
+	
+	//Char can be used to stare single alphabets
+	char g = 'h';
+	
+	//boolean is used to store true or false statements
+	boolean i = true;
+	
+	//String is used to store words
+	String str ;
+	
+	
+}
+}
